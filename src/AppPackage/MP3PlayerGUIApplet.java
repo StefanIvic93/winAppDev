95d082b32d69f8fdc0a3466fe1e51cefa1d7c6b8
Stefan Ivic <stefaivic93@outlook.com>
2020-03-10 17:07:23 +0100

    mp3widgetpractice commit

---
+++ src/AppPackage/MP3PlayerGUIApplet.java
@@ -0,0 +1,128 @@
+
+package AppPackage;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.JApplet;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+/**
+ *
+ * @author Ivke
+ */
+public class MP3PlayerGUIApplet extends JApplet {
+
+    
+  FileInputStream FIS;
+  BufferedInputStream BIS;
+  
+  public Player player;
+  
+  public long pauseLocation;
+  public long songTotalLenght;
+  public String fileLocation;
+  
+  public void stop()
+  {
+   if(player != null)
+   {
+       player.close();
+       pauseLocation = 0;
+       songTotalLenght = 0;
+       
+       MP3PlayerGUI.Display.setText("");
+   }             
+  }
+  public void Pause()
+  {
+   if(player != null)
+   {
+       try {
+           pauseLocation = FIS.available();
+           player.close();
+       } catch (IOException ex) {
+           
+       }
+       player.close();
+   }             
+  }
+  
+  public void Resume()
+{
+      try {
+          FIS = new FileInputStream(fileLocation);
+          BIS = new BufferedInputStream(FIS);
+          try {
+              player = new Player(BIS);
+              FIS.skip(songTotalLenght - pauseLocation); 
+          } catch (JavaLayerException ex) {
+             
+          } catch (IOException ex) {
+              
+          }
+           
+      } catch (FileNotFoundException ex ) 
+      {
+    
+      }
+      
+      new Thread()
+      {
+          @Override
+          public void run()
+          {
+              try {
+                  player.play();
+              } catch (JavaLayerException ex) {
+                  
+                 
+              }
+          }
+      }.start();
+}
+  
+public void Play(String path)
+{
+      try {
+          
+          FIS = new FileInputStream(path);
+          BIS = new BufferedInputStream(FIS);
+          try {
+              player = new Player(BIS);
+             songTotalLenght = FIS.available();
+             fileLocation = path + "";
+          } catch (JavaLayerException ex) {
+             
+          } catch (IOException ex) {
+          
+          }
+           
+      } catch (FileNotFoundException ex ) 
+      {
+    
+      }
+      
+      new Thread()
+      {
+          @Override
+          public void run()
+          {
+              try {
+                  player.play();
+                  if(player.isComplete() && MP3PlayerGUI.count == 1)
+                  {
+                      Play(fileLocation);
+                  }
+              } catch (JavaLayerException ex) {
+                  
+                 
+              }
+          }
+      }.start();
+}
+}
+
+  
